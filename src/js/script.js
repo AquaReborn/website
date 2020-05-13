6e48da4ae11cb5857f1ad063ecb7cf4c29eb7f40
Morrow <44026536+SkyterStrike@users.noreply.github.com>
2020-05-13 14:10:43 +0200

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,3 @@
 window.on("load", e => {
-    const header = document.find("header")
-    const nav = document.find("nav")
-    const section = document.find("section")
-
-    window.on("scroll", e => {
-        if (window.scrollY > header.clientHeight) {
-            nav.classList.add("fixed")
-            section.style.marginTop = "41px"
-        } else {
-            nav.classList.remove("fixed")
-            section.style.marginTop = "10px"
-        }
-    })
+    // code JS
 })
